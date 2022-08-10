26d6173aeecc00bd6acad0fee33f9c59daaa2568
Ana Carolina Fernandes da Silva <ana.fersilva@hotmail.com>
2022-08-09 22:27:07 -0300

    init

--- siteLivraria/Controllers/HomeController.cs
+++ siteLivraria/Controllers/HomeController.cs
@@ -6,13 +6,14 @@ namespace siteLivraria.Controllers
 {
     public class HomeController : Controller
     {
+        /*
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
+        */
         public IActionResult Index()
         {
             return View();
