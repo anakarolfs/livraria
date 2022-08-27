795e94124123e32d5440e8e22806a36b52286aa8
Ana Carolina Fernandes da Silva <ana.fersilva@hotmail.com>
2022-08-27 15:02:05 -0300

    removendo SJP

--- siteLivrariaSJP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace siteLivrariaSJP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
